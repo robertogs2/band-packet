e82cd7b75ccc76bf7b941b5528a02cd2901a8e4f
JosafatV <josav09@gmail.com>
2019-10-10 12:19:50 -0600

    modified packets to correct behavior, added config file reading

--- packets/include/packageGenerator.h
+++ packets/include/packageGenerator.h
@@ -1,14 +1,14 @@
 #ifndef PACKAGE_GENERATOR
 #define PACKAGE_GENERATOR
 
-#include <math.h>
-#include <time.h> 
+// reads band info from a conf file
+config_t get_config(const char* conf_path);
 
 // creates a pseudo-random number that follows a distribution
 int randNum (int mean, int stdDev, int distro);
 
 // instantiates a package and increments the id counter
-void createPackage(int* packageCounter, package_t* newPackage, int pRads, int pUrge, int pLeft, int pSignBand, int pRandomBand);
+void createPackage(int* packageCounter, package_t* newPackage, int pSignBand, int pRandomBand);
 
 // prints the values of the package struct
 void checkPackage(package_t* testPackage);
