a0c7c5e04158c600dc0ba89cbd06e3ad8e74e37b
Roberto <roberm98a@gmail.com>
2019-10-12 16:08:39 -0600

    nose

--- schedulers/include/band_control.h
+++ schedulers/include/band_control.h
@@ -5,45 +5,51 @@
 #include "linked_list.h"
 #include "constants.h"
 
-struct side_controller{
+typedef struct{
 	int amount_moved;
 	short last_side;
 	short sign;
 	int W;
 
-	Node_t* left_list;
-	Node_t* right_list;
+	Node_t** left_list;
+	Node_t** right_list;
 	short type;
-};
+}side_controller_t;
 
 short flip(short number){
-	1-number;
+	return 1-number;
 }
 
-int get_list_size(short side, struct side_controller* controller){
-	if(side == 0) return get_length(controller->left_list);
-	else return get_length(controller->right_list);
+int get_list_size(short side, side_controller_t* controller){
+	if(side == 0) return get_length(*controller->left_list);
+	else return get_length(*controller->right_list);
 }
 
-void init_controller(struct side_controller* controller, Node_t* left_list, Node_t* right_list, short type){
+int get_list_size2(Node_t** list){
+	return get_length(*list);
+}
+
+void init_controller(side_controller_t* controller, Node_t** left_list, Node_t** right_list, short type){
 	controller->amount_moved = 0;
 	controller->last_side = 0;
 	controller->left_list = left_list;
 	controller->right_list = right_list;
 	controller->sign = 0;
 	controller->type = type;
-	controller->W = 0;
+	controller->W = 2; //fixed
 }
 // 0 on left, 1 on right
-short control_band(struct side_controller* controller){
+short control_band(side_controller_t* controller){
 	short type = controller->type;
 		if(type == W_BAND){
-			if(controller->amount_moved >= controller->W){ // Reached limit
+			//printf("Using W_BAND\n");
+			if(controller->amount_moved >= controller->W-1){ // Reached limit
+
 				controller->last_side = flip(controller->last_side); // Changes side
 				int new_side_length = get_list_size(controller->last_side, controller);
 				if(new_side_length == 0){
 					controller->last_side = flip(controller->last_side); // New side has no packages
-					++controller->amount_moved; // Keep going to enter here everytime
+					controller->amount_moved++; // Keep going to enter here everytime
 				}
 				else{ // No problem with new side
 					controller->amount_moved = 0;
@@ -59,16 +65,28 @@ short control_band(struct side_controller* controller){
 			return controller->last_side;
 		}
 		else if(type == SIGN_BAND){
+			printf("Using SIGN\n");
 			int new_side_length = get_list_size(controller->sign, controller); // Side from list to give
-			if(new_side_length > 0) return controller->sign;		// If it has packages give on
-			return flip(controller->sign);							// If not, give another
+			if(new_side_length > 0) controller->last_side = controller->sign;		// If it has packages give on
+			else controller->last_side = flip(controller->sign);							// If not, give another
+			return controller->last_side;
 		}
 		else if(type == RANDOM_BAND){
+			printf("Using RANDOM_BAND\n");
 			int n = rand()%101;
+			
 			short dir = n > 50;
 			int new_side_length = get_list_size(dir, controller); // Side from list to give
-			if(new_side_length > 0) return dir;		// If it has packages give on
-			return flip(dir);							// If not, give another
+			
+			if(new_side_length > 0) {
+				printf("%d\n", dir);
+				controller->last_side = dir; // If it has packages give on
+			}
+			else{
+				controller->last_side = flip(dir);
+				printf("%d\n", flip(dir)); // If not, give another try
+			}
+			return controller->last_side;
 		}
 		return -1;
 }
